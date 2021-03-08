863bf87e8ef14ec3cc81dc70224f5ffe3807911c
SafiyaMGeorge <SMGeorge2@fullsail.com>
2021-03-07 21:52:45 -0500

    This code demonstrates how I will get data from the rapid API movie database.

---
+++ movieApp.js
@@ -0,0 +1,13 @@
+
+fetch("https://movie-database-imdb-alternative.p.rapidapi.com/?s=Avengers%20Endgame&page=1&r=json", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "8972c48833msh0476b30d983b365p101eccjsn5731aba096ef",
+		"x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com"
+	}
+})
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => {
+	console.error(err);
+});
